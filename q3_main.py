4b539a64328a3bdc4a12b536f65a1768892fff03
isabelvictoriaojeda <isabelvictoriaojeda@gmail.com>
2025-03-11 20:48:15 -0400

    q3 complete

---
+++ q3_main.py
@@ -0,0 +1,41 @@
+# Isabel Ojeda
+# HW5 Q3 
+
+from question3 import Student, Professor, TA, Course
+if __name__ == '__main__': 
+    # course object
+    course = Course("Programming Paradigms", "CSE30332")
+    
+    # professor object
+    professor = Professor("Prof. Santos", "100", "jdasilv2@nd.edu", "CSE")
+    course.add_instructor(professor)
+    
+    # TA object
+    ta1 = TA("Micah Brody", "T1", "mbrody@nd.edu")
+    ta1.assign_to_courses(course)
+    ta2 = TA("Sahil Khandelwal", "T2", "skhandel@nd.edu")
+    ta2.assign_to_courses(course)
+    ta3 = TA("Kaixiang Zhao", "T3", "kzhao5@nd.edu")
+    ta3.assign_to_courses(course)
+    ta4 = TA("Ella Gerzack", "T4", "egerczak@nd.edu")
+    ta4.assign_to_courses(course)
+    
+    # student objects
+    student1 = Student("Isabel Ojeda", "S1", "iojeda@nd.edu", "CSE")
+    course.add_student(student1)
+    student2 = Student("Phoebe Huang", "S2", "phuang@nd.edu", "CSE")
+    course.add_student(student2)
+    student3 = Student("Sofia Rexach", "S3", "srexach@nd.edu", "CPEG")
+    course.add_student(student3)
+
+    print("\n")
+    print(course)
+    print("\n")
+    print(professor)
+    print("\n")
+    print("TAS:")
+    for ta in [ta1, ta2, ta3, ta4]:
+        print(ta)
+    print("\nStudents:")
+    for s in [student1, student2, student3]:
+        print(s)
--- question3.py
+++ question3.py
@@ -34,7 +34,38 @@ class Professor(UniversityMember):
         return f"Prof {last_name} ({self.email})"
     
 class TA(UniversityMember):
-    def __init__(self, name, member_id, email, courses_assisting):
-        self.courses_assisting = []
+    def __init__(self, name, member_id, email):
+        super().__init__(name, member_id, email)
+        self.courses_assisting = [] # attribute 
     def get_role(self):
         "TA"
+    # method that takes as input a course and add to the field courses_assisting.
+    def assign_to_courses(self, course):
+        self.courses_assisting.append(course)
+    def __str__(self):
+        courses_list = ", ".join([c.name for c in self.courses_assisting])
+        return f"{self.name} ({self.email}) TA for courses: {courses_list}"
+
+# does not inherit from university member 
+class Course: 
+    def __init__(self, name, code):
+        # instructors and students are added in methods 
+        self.name = name
+        self.code = code
+        self.enrolled_students = []
+        self.instructor = None 
+    def add_student(self, student):
+        # check if student belongs to Student class 
+        if isinstance(student, Student):
+            self.enrolled_students.append(student)
+    def remove_student(self, student):
+        if student in self.enrolled_students:
+            self.enrolled_students.remove(student)
+    def add_instructor(self, professor):
+        if isinstance(professor, Professor):
+            self.instructor = professor
+    def remove_instructor(self, professor):
+        self.instructor = None 
+    def __str__(self):
+        students_list = ", ".join([s.name for s in self.enrolled_students])
+        return f"Course: {self.name} ({self.code})\n Instructor: {self.instructor}\n Students: {students_list}"
